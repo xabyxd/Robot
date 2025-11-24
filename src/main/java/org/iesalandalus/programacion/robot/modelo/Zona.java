4ad9e9644b9ca4a286900865c4608334079706aa
xabyxd <xabybrad@gmail.com>
2025-11-24 14:03:39 +0100

    Creado el Enum "Orientación"

---
+++ src/main/java/org/iesalandalus/programacion/robot/modelo/Zona.java
@@ -0,0 +1,50 @@
+package org.iesalandalus.programacion.robot.modelo;
+
+import java.util.Objects;
+
+public record Zona(int ancho, int alto) {
+    public static final int ANCHO_MINIMO = 10;
+    public static final int ANCHO_MAXIMO = 100;
+    public static final int ALTO_MINIMO = 10;
+    public static final int ALTO_MAXIMO = 100;
+
+    public Zona {
+        validarAncho(ancho);
+        validarAlto(alto);
+    }
+
+    public Zona() { this(
+            ANCHO_MINIMO,
+            ALTO_MINIMO
+    ); }
+
+    private void validarAncho(int ancho) {
+        if (ancho < ANCHO_MINIMO || ancho > ANCHO_MAXIMO) {
+            throw new IllegalArgumentException("Ancho no válido.");
+        }
+    }
+
+    private void validarAlto(int alto) {
+        if (alto < ALTO_MINIMO || alto > ALTO_MAXIMO) {
+            throw new IllegalArgumentException("Alto no válido.");
+        }
+    }
+
+    public Coordenada getCentro() {
+        return new Coordenada(ancho/2, alto/2);
+    }
+
+    public boolean pertenece(Coordenada coordenada) {
+        Objects.requireNonNull(coordenada, "La coordenada no puede ser nula.");
+        return (perteneceX(coordenada.x()) && perteneceY(coordenada.y()));
+    }
+
+    private boolean perteneceX(int x) {
+        return (x < ancho && x >= 0);
+    }
+
+    private boolean perteneceY(int y) {
+        return (y < alto && y >= 0);
+    }
+
+}
